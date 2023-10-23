b1c43ec6d27b0b5f3736db5b60b3780a3848016d
Pham Van Nguyen <nguyenpv@fint.vn>
2023-10-23 17:43:50 +0700

    code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,62 @@
-
+import * as wjCore from '@grapecity/wijmo';
+import * as wjGrid from '@grapecity/wijmo.grid';
 
 import { Component, Inject } from '@angular/core';
-import * as wjGrid from '@grapecity/wijmo.grid';
 import { DataService, DataItem } from './app-data';
-import * as wjCore from '@grapecity/wijmo';
+import { reportConfigurations } from './report-config'; // Đảm bảo rằng đường dẫn này chính xác
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
     data: DataItem[];
+    headers: any[] = [];
+    reportName = 'bao-cao-tu-thiet-bi'; 
 
     constructor(@Inject(DataService) private dataService: DataService) {
         this.data = dataService.getData();
+        
+        if (this.data.length && reportConfigurations[this.reportName]) {
+            const config = reportConfigurations[this.reportName];
+            this.headers = config.headers.map(header => {
+
+                return {
+                    binding: header,
+                    header: header.charAt(0).toUpperCase() + header.slice(1),
+                    allowMerging: header === config.mergedColumns.label
+                };
+            });
+        }
     }
 
     onInitialized(grid: wjGrid.FlexGrid) {
-      // create extra header row
-      var extraRow = new wjGrid.Row();
-      extraRow.allowMerging = true;
-      //
-      // add extra header row to the grid
-      var panel = grid.columnHeaders;
-      panel.rows.splice(0, 0, extraRow);
-  
-      for (let colIndex = 1; colIndex <= 2; colIndex++) {
-          panel.setCellData(0, colIndex, 'Amounts');
-      }
-      //
-      // merge "Country" and "Active" headers vertically
-      ['country', 'active'].forEach(function (binding) {
-          let col = grid.getColumn(binding);
-          col.allowMerging = true;
-          panel.setCellData(0, col.index, col.header);
-      });
-      //
-      // center-align merged header cells
-      grid.formatItem.addHandler(function (s: wjGrid.FlexGrid, e: wjGrid.FormatItemEventArgs) {
-        if (e.panel == s.columnHeaders && e.range.row == 0 && (e.col == 1 || e.col == 2)) {
-            wjCore.addClass(e.cell, 'centered-content');
+        const config = reportConfigurations[this.reportName];
+        if (config) {
+            // create extra header row
+            var extraRow = new wjGrid.Row();
+            extraRow.allowMerging = true;
+
+            // add extra header row to the grid
+            var panel = grid.columnHeaders;
+            panel.rows.splice(0, 0, extraRow);
+
+            // apply mergedColumns from config
+            for (let i = config.mergedColumns.start; i <= config.mergedColumns.end; i++) {
+                let col = grid.columns[i];
+                if (col) {
+                    col.allowMerging = true;
+                }
+                panel.setCellData(0, i, config.mergedColumns.label);
+            }
+
+            grid.formatItem.addHandler(function (s: wjGrid.FlexGrid, e: wjGrid.FormatItemEventArgs) {
+                if (e.panel == s.columnHeaders && e.range.rowSpan > 1) {
+                    var html = e.cell.innerHTML;
+                    e.cell.innerHTML = '<div class="v-center">' + html + '</div>';
+                }
+            });
         }
-    });
-    
-  }
+    }
 }
